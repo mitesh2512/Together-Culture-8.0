cf6843bbc63e764a54f30b18844157da7e28b8d2
Adit Baldva <AB2574@student.aru.ac.uk>
2024-11-11 15:35:17 +0000

    Admin Profile

--- Together Culture/Admin_Profile.Designer.cs
+++ Together Culture/Admin_Profile.Designer.cs
@@ -34,52 +34,146 @@
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(153, 84);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(60, 105);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(114, 27);
+            textBox1.Size = new Size(233, 23);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(453, 99);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(319, 105);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(114, 27);
+            textBox2.Size = new Size(203, 23);
             textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(343, 211);
-            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Location = new Point(60, 214);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(114, 27);
+            textBox3.Size = new Size(324, 23);
             textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(153, 211);
-            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Location = new Point(60, 158);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(114, 27);
+            textBox4.Size = new Size(462, 23);
             textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(404, 214);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(118, 23);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(60, 271);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(462, 23);
+            textBox6.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 6;
+            label1.Text = "First Name";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(320, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(60, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Email Address";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(60, 193);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Address";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(402, 195);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Postcode";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(62, 252);
+            label6.Name = "label6";
+            label6.Size = new Size(96, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Contact Number";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(572, 53);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 100);
+            pictureBox1.TabIndex = 12;
+            pictureBox1.TabStop = false;
             // 
             // Admin_Profile
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 451);
+            ClientSize = new Size(700, 338);
+            Controls.Add(pictureBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Admin_Profile";
             Text = "Admin Profile";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -90,5 +184,14 @@
         private TextBox textBox2;
         private TextBox textBox3;
         private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private PictureBox pictureBox1;
     }
 }
--- Together Culture/Admin_Profile.cs
+++ Together Culture/Admin_Profile.cs
@@ -16,5 +16,15 @@ namespace Together_Culture
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
