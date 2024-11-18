65c9ec81b432608181c2519d80a89c1c9081887b
Mitesh Patel <MPP129@student.aru.ac.uk>
2024-11-18 13:34:50 +0000

    database connection

--- Together Culture/Login_Page .cs
+++ Together Culture/Login_Page .cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Together_Culture
 {
@@ -74,6 +76,20 @@ namespace Together_Culture
 
         private void Login_Button(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\MitesH\\source\\repos\\mitesh2512\\Together-Culture-8.0\\Together Culture\\DataBae\\together.db.mdf\";Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            string query = "SELECT COUNT(*) FROM Try WHERE email=@Email AND password=@Password";
+            SqlCommand cmd = new SqlCommand(query,con); 
+            cmd.Parameters.AddWithValue("@email", UserName.Text);
+            cmd.Parameters.AddWithValue("@password", textBox2.Text);
+            int count = (int)cmd.ExecuteScalar();
+            con.Close();
+            if (count > 0)
+            {
+                Remember.Text = "Success";
+            }
+
+
 
         }
 
